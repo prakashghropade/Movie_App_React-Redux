5e8a7be210fae4ccf61f95f867e47f65510bd2e6
prakashghorpade <prakashghorpade2001@gmail.com>
2024-07-16 11:42:40 +0530

    sliding image function completed

--- src/components/BannerHome.js
+++ src/components/BannerHome.js
@@ -1,35 +1,90 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+import { FaAnglesRight } from "react-icons/fa6";
+import { FaAnglesLeft } from "react-icons/fa6";
+
 
 const BannerHome = () => {
 
   const bannerData = useSelector(state => state.movieoData.bannerData);
 
-  const imageURL = useSelector(state => state.movieoData.imageURL)
+  const imageURL = useSelector(state => state.movieoData.imageURL);
+
+  const [currentImage, setCurrentImage]  = useState(1);
+
+
+  const handleNext = () => {
+         if(currentImage < bannerData.length - 1){
+          setCurrentImage(prev => prev + 1)
+         }
+  }
+
+  const handlePrevious = () => {
+    if(currentImage > 0){
+      setCurrentImage(prev => prev - 1)
+     }
+  }
+
+  useEffect(() => {
+       const interval = setInterval(() => {
+        if(currentImage < bannerData.length - 1){
+           handleNext();
+         }
+         else{
+          setCurrentImage(0);
+         } 
+       }, 5000)
+
+       return () => clearInterval(interval)
+  }, [bannerData, imageURL])
 
   return (
     <section className='w-full h-full'>
-      <div className='flex min-h-full max-h-[95vh]'>
+      <div className='flex min-h-full max-h-[95vh] overflow-hidden'>
         {
           bannerData.map((data, index) => {
             return (
-              <div className='min-w-full min-h-[450px] lg:min-h-full overflow-hidden relative'>
+              <div className='min-w-full min-h-[450px] lg:min-h-full overflow-hidden relative group transition-all duration-700' style={{transform: `translateX(-${currentImage * 100}%)`}} key={index}>
 
-                <div key={index} className='w-full h-full'>
+                <div  className='w-full h-full'>
                   <img
                     src={imageURL + data.backdrop_path}
                     className='h-full w-full object-cover'
                   />
                 </div>
 
-                <div>
+
+                {/* button next and previous image */}
+                <div className='absolute top-0 w-full h-full hidden items-center justify-between p-4 group-hover:lg:flex'>
+
+                  <button className='bg-white p-1 rounded-full text-2xl z-10 text-black' onClick={handlePrevious}>
+                  <FaAnglesLeft />
+                  </button>
+
+                  <button className='bg-white p-1 rounded-full text-2xl z-10 text-black' onClick={handleNext}>
+                  <FaAnglesRight />
+                  </button>
+
+                </div>
+
+                
                 <div className='absolute top-0 w-full h-full bg-gradient-to-t from-neutral-900 to-transparent'> </div>
-                </div>
+               
 
-                <div className='container max-auto absolute bottom-0 max-w-md'>
-                      <h2>{data.title}</h2>
-                </div>
+                 <div className='container mx-auto'>
+                <div className=' w-full absolute bottom-0 max-w-md px-3'>
+                      <h2 className='font-bold text-2xl lg:text-4xl text-white drop-shadow-3xl'>{data?.title || data?.name}</h2>
+                      <p className='text-ellipsis line-clamp-3 my-2'>{data.overview}</p>
 
+                      <div className='flex items-center gap-4'>
+                        <p>Rating: {Number(data.vote_average).toFixed(1)}</p>
+                        <span>|</span>
+                        <p>View: {Number(data.popularity).toFixed(0)}</p>
+                      </div>
+
+                      <button className='bg-white px-4 py-2 text-black rounded mt-4 hover:bg-gradient-to-l from-red-700 to-orange-500 shadow-md transition-all hover:scale-105'>Play Now</button>
+                </div>
+               </div>
               </div>
             )
           })
